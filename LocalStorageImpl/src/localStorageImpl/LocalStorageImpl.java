7eba95d78d1cea02f3571e69cf1416357bd5676a
DusanGligorijevic <dukacode@gmail.com>
2021-11-02 13:23:54 +0100

    prvi

---
+++ LocalStorageImpl/src/localStorageImpl/LocalStorageImpl.java
@@ -0,0 +1,33 @@
+package localStorageImpl;
+
+import java.io.File;
+
+import storage.Storage;
+
+public class LocalStorageImpl extends Storage{
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transfer(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preview(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
