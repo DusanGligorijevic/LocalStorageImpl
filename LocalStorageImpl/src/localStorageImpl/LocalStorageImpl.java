48ae2243c8baba55737e924f79f3a2600b067999
DusanGligorijevic <dukacode@gmail.com>
2021-11-02 22:22:00 +0100

    -

--- LocalStorageImpl/src/localStorageImpl/LocalStorageImpl.java
+++ LocalStorageImpl/src/localStorageImpl/LocalStorageImpl.java
@@ -3,12 +3,34 @@ package localStorageImpl;
 import java.io.File;
 
 import storage.Storage;
+import storage.StorageManager;
 
 public class LocalStorageImpl extends Storage{
+	
+	static {
+		StorageManager.registerStorage(new LocalStorageImpl());
+		
+	}
+	
+	public LocalStorageImpl() {
+		
+	}
 
 	@Override
-	public void create() {
-		// TODO Auto-generated method stub
+	public void create(String path, int maxFolders) {
+		String name ="file";
+		for(int i = 0; i < maxFolders; i++) {
+			
+			//Instantiate the File class   
+            File storage = new File(StoragePath+"\\"+name+i);  
+            //Creating a folder using mkdir() method  
+            boolean bool = storage.mkdir();  
+            if(bool){  
+               System.out.println("folder "+name+i+" je uspesno kreiran!");  
+            }else{  
+               System.out.println("Greska pri kreiranju foldera "+name);  
+            }
+		}
 		
 	}
 
