339ac550e0603b4323d1fca804f8255661e4f88d
Laki <Laki@DESKTOP-HRL91L7>
2021-11-09 01:48:47 +0100

    Override starih i novih metoda.

--- LocalStorageImpl/src/localStorageImpl/LocalStorageImpl.java
+++ LocalStorageImpl/src/localStorageImpl/LocalStorageImpl.java
@@ -1,9 +1,12 @@
 package localStorageImpl;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import storage.Storage;
 import storage.StorageManager;
+import localStorageImpl.*;
 
 public class LocalStorageImpl extends Storage{
 	
@@ -35,21 +38,77 @@ public class LocalStorageImpl extends Storage{
 	}
 
 	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
-		
+	public void delete(String path) {
+		File f = new File(path);
+		if(f.delete()) {
+			System.out.println("Folder je izbrisan");
+		}else {
+			System.out.println("Folder nije izbrisan");
+		}		
 	}
 
 	@Override
-	public void transfer(File f) {
-		// TODO Auto-generated method stub
-		
+	public void transfer(String location,String destination,String file) {
+		File folder = new File(location);
+		File [] prevFiles=folder.listFiles();
+		for(File f:prevFiles) {
+				File file1 = new File(file);
+				if(file1.exists()) {
+					File dest = new File(destination);
+					try {
+						Files.copy(file1.toPath(), dest.toPath(),StandardCopyOption.REPLACE_EXISTING);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}else
+					System.out.println("Ne postoji folder!");	
+		}			
 	}
 
+	@Override
+	public void preview(File f,String extension) {
+		File [] prevFiles=f.listFiles();
+		System.out.println("Trazeni fajlovi:");
+		for(File file:prevFiles) {
+			if(f.getName().contains(extension)) {
+				System.out.println(f.getName());
+				
+			}
+		}		
+	}
+	
 	@Override
 	public void preview(File f) {
-		// TODO Auto-generated method stub
-		
+		File [] prevFiles=f.listFiles();
+		System.out.println("Trazeni fajlovi:");
+		for(File file:prevFiles) {
+				System.out.println(f.getName());
+				
+			
+		}		
+	}
+	
+	@Override
+	public void preview(File f,boolean directoriesOnly) {
+		File [] prevFiles=f.listFiles();
+		System.out.println("Trazeni fajlovi:");
+		if (directoriesOnly==true) {
+			for(File file:prevFiles) {
+				if(f.isDirectory()) {
+					System.out.println(f.getName());
+					
+				}
+			}
+		}else {
+			for(File file:prevFiles) {
+				if(f.isDirectory()==false) {
+					System.out.println(f.getName());
+					
+				}
+			}
+			
+		}
+	
 	}
 
 }
